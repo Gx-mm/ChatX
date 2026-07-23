fcde9d2a6ecbeb7d245768c3609857c02902b9e9
Gx-mm <sknravi290@gmail.com>
2026-07-23 21:28:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL='https://uaawwwdyjogseeanzfav.supabase.co';
-const SUPABASE_ANON_KEY='PASTE_YOUR_ANON_KEY_HERE';
-const supabase=window.supabase.createClient(SUPABASE_URL,SUPABASE_ANON_KEY);
-document.querySelector('.google').onclick=async()=>{
-const {error}=await supabase.auth.signInWithOAuth({provider:'google',options:{redirectTo:'https://gx-mm.github.io/ChatX/'}});
-if(error)alert(error.message);
-};
