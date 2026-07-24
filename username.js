aef8c2510c89d532f0a7532069e76928087d97ba
Gx-mm <sknravi290@gmail.com>
2026-07-24 13:28:13 +0530

    Implement back button functionality
    Added event listener for back button to navigate to index.html.

--- username.js
+++ username.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    document.getElementById("backBtn").addEventListener("click", function () {
+    window.location.href = "index.html";
+});
     const SUPABASE_URL = 'https://uaawwwdyjogseeanzfav.supabase.co';
     const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYXd3d2R5am9nc2VlYW56ZmF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2MzYwNzcsImV4cCI6MjEwMDIxMjA3N30.gjDWOzU1O1sXH3VcP1IIEUp0uQzpRdF_xzzZ9Rwxluk';
     
